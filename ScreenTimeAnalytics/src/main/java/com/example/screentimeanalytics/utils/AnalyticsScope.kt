eac07d820de9418ffb2467f7513f6f54ae735de3
Toshika Badjatya <77146844+ToshikaBadjatya@users.noreply.github.com>
2026-02-21 20:47:20 +0530

    generate dynamic base class runtime

--- ScreenTimeAnalytics/src/main/java/com/example/screentimeanalytics/utils/AnalyticsScope.kt
+++ ScreenTimeAnalytics/src/main/java/com/example/screentimeanalytics/utils/AnalyticsScope.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 
-internal object AnalyticsScope {
+object AnalyticsScope {
     val scope = CoroutineScope(
         SupervisorJob() + Dispatchers.IO
     )
